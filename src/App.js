8ad15463826cfd3eeaeb3fc9ffacdfbca2a966bd
UDAY368 <udayabdm368@gmail.com>
2023-06-30 10:42:45 +0530

    Create a first react Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Employee from './components/Employee';
 
 function App() {
+  const isShowEmployee = true
   return (
-    <div className="App">
-      <h1>Hello world of React JS</h1>
+    <div>
+      { isShowEmployee ? <>
+       <Employee />
+       <Employee />
+       <Employee />
+      </> : <p>There is no Employee to Display</p>}
     </div>
   );
 }
---
+++ src/components/Employee.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Employee() {
+  return (
+    <div>
+      <h1>This is the Employee Component</h1>
+    </div>
+  )
+}
+
+export default Employee
