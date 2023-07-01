23db8d5ad9f2a1d420bc3c31a19659cfa52b151b
UDAY368 <udayabdm368@gmail.com>
2023-07-01 17:33:18 +0530

    Add the pages and pass the children props

--- src/App.js
+++ src/App.js
@@ -1,107 +1,12 @@
-import { useState } from "react";
-import "./index.css";
-import Employee from "./components/Employee";
-import AddEmployee from "./components/AddEmployee";
-import { v4 as uuidv4 } from "uuid";
-import EditEmployee from "./components/EditEmployee";
 import Header from "./components/Header";
+import "./index.css";
+import Employees from "./pages/Employees";
 
 function App() {
-  // Perfect usage of the useState Hook
-  const [employees, setemployees] = useState([
-    {
-      id: 1,
-      name: "uday kumar",
-      role: "product Engineer",
-      img: "https://images.pexels.com/photos/2955376/pexels-photo-2955376.jpeg",
-    },
-    {
-      id: 2,
-      name: "srinu Uppati",
-      role: "Product Marketing",
-      img: "https://images.pexels.com/photos/756484/pexels-photo-756484.jpeg",
-    },
-    {
-      id: 3,
-      name: "Shakeel",
-      role: "production Lead",
-      img: "https://images.pexels.com/photos/2955376/pexels-photo-2955376.jpeg",
-    },
-    {
-      id: 4,
-      name: "Usha Rani",
-      role: "Front End Developer",
-      img: "https://images.pexels.com/photos/7897404/pexels-photo-7897404.jpeg",
-    },
-    {
-      id: 5,
-      name: "Samyuktha Sammu",
-      role: "HR Relations",
-      img: "https://images.pexels.com/photos/7897404/pexels-photo-7897404.jpeg",
-    },
-    {
-      id: 6,
-      name: "Prasanna prassu",
-      role: "Java Developer",
-      img: "https://images.pexels.com/photos/7897404/pexels-photo-7897404.jpeg",
-    },
-  ]);
-  const isShowemployee = true;
-
-  function updateEmployee(id, newName, newRole) {
-    const updateEmployees = employees.map((employee) => {
-      if (id === employee.id) {
-        return { ...employee, name: newName, role: newRole };
-      }
-
-      return employee;
-    });
-    setemployees(updateEmployees);
-  }
-
-  function addEmployee(name, role, image) {
-    const newEmployee = {
-      id: uuidv4(),
-      name: name,
-      role: role,
-      img: image,
-    };
-    setemployees([...employees, newEmployee]);
-  }
-
   return (
-    <div className="App  bg-gray-300 min-h-screen">
-      <Header />
-      {isShowemployee ? (
-        <>
-          <div className=" flex flex-wrap justify-center mt-3">
-            {employees.map((employee) => {
-              const editEmployee = (
-                <EditEmployee
-                  name={employee.name}
-                  role={employee.role}
-                  id={employee.id}
-                  updateEmployee={updateEmployee}
-                />
-              );
-              return (
-                <Employee
-                  key={employee.id}
-                  id={employee.id}
-                  name={employee.name}
-                  role={employee.role}
-                  img={employee.img}
-                  updateEmployee={editEmployee}
-                />
-              );
-            })}
-          </div>
-          <AddEmployee addEmployee={addEmployee} />
-        </>
-      ) : (
-        <p>There is no employeee to Display</p>
-      )}
-    </div>
+    <Header>
+      <Employees />
+    </Header>
   );
 }
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Disclosure } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const navigation = [
@@ -13,13 +13,13 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function Header() {
+export default function Header(props) {
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-14 items-center justify-between">
+            <div className="relative flex h-12 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -84,6 +84,7 @@ export default function Header() {
               ))}
             </div>
           </Disclosure.Panel>
+          {props.children}
         </>
       )}
     </Disclosure>
