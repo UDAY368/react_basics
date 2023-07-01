fbe633362dbd47ecaf2f7965847d35159e6090f1
UDAY368 <udayabdm368@gmail.com>
2023-07-01 14:20:09 +0530

    Complete the Update employee functionality

--- src/App.js
+++ src/App.js
@@ -1,78 +1,92 @@
 import { useState } from "react";
 import "./index.css";
 import Employee from "./components/Employee";
-import { v4 as uuidv4 } from "uuid";
 
 function App() {
   // Perfect usage of the useState Hook
   const [employees, setemployees] = useState([
     {
+      id: 1,
       name: "uday kumar",
       role: "product Engineer",
       img: "https://images.pexels.com/photos/2955376/pexels-photo-2955376.jpeg",
     },
     {
+      id: 2,
       name: "srinu Uppati",
       role: "Product Marketing",
       img: "https://images.pexels.com/photos/756484/pexels-photo-756484.jpeg",
     },
     {
+      id: 3,
       name: "Shakeel",
       role: "production Lead",
       img: "https://images.pexels.com/photos/2955376/pexels-photo-2955376.jpeg",
     },
     {
+      id: 4,
       name: "Usha Rani",
       role: "Front End Developer",
       img: "https://images.pexels.com/photos/7897404/pexels-photo-7897404.jpeg",
     },
     {
+      id: 5,
       name: "Samyuktha Sammu",
       role: "HR Relations",
       img: "https://images.pexels.com/photos/7897404/pexels-photo-7897404.jpeg",
     },
     {
+      id: 6,
       name: "Prasanna prassu",
       role: "Java Developer",
       img: "https://images.pexels.com/photos/7897404/pexels-photo-7897404.jpeg",
     },
     {
+      id: 7,
       name: "Shankar",
       role: "python Developer",
       img: "https://images.pexels.com/photos/2955376/pexels-photo-2955376.jpeg",
     },
     {
+      id: 8,
       name: "Prasad",
       role: "employeee Relations",
       img: "https://images.pexels.com/photos/2955376/pexels-photo-2955376.jpeg",
     },
     {
+      id: 9,
       name: "Chiranjeevi",
       role: "CEO",
       img: "https://images.pexels.com/photos/2955376/pexels-photo-2955376.jpeg",
     },
   ]);
   const isShowemployee = true;
+
+  function updateEmployee(id, newName, newRole) {
+    const updateEmployees = employees.map((employee) => {
+      if (id === employee.id) {
+        return { ...employee, name: newName, role: newRole };
+      }
+
+      return employee;
+    });
+    setemployees(updateEmployees);
+  }
+
   return (
     <div className="App">
       {isShowemployee ? (
         <>
-          {/* <input
-            type="text"
-            placeholder="Enter Role"
-            className="p-1"
-            onChange={(e) => {
-              setRole(e.target.value);
-            }}
-          /> */}
           <div className=" flex flex-wrap justify-center">
             {employees.map((employee) => {
               return (
                 <Employee
-                  key={uuidv4()}
+                  key={employee.id}
+                  id={employee.id}
                   name={employee.name}
                   role={employee.role}
                   img={employee.img}
+                  updateEmployee={updateEmployee}
                 />
               );
             })}
--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import Modal from "react-bootstrap/Modal";
 
-function EditEmployee() {
+function EditEmployee(props) {
+  const [name, setName] = useState(props.name);
+  const [role, setRole] = useState(props.role);
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -13,7 +15,7 @@ function EditEmployee() {
         onClick={handleShow}
         className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
       >
-        Update
+        Edit
       </button>
 
       <Modal
@@ -27,42 +29,53 @@ function EditEmployee() {
         </Modal.Header>
         <Modal.Body>
           <form
+            onSubmit={(e) => {
+              handleClose();
+              e.preventDefault();
+              props.updateEmployee(props.id, name, role);
+            }}
             id="editmodal"
-            class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
           >
-            <div class="md:flex md:items-center mb-6">
-              <div class="md:w-1/3">
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
                 <label
-                  class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                  for="name"
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  htmlFor="name"
                 >
                   Name
                 </label>
               </div>
-              <div class="md:w-2/3">
+              <div className="md:w-2/3">
                 <input
-                  class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                   id="name"
                   type="text"
-                  value="Jane Doe"
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
                 />
               </div>
             </div>
-            <div class="md:flex md:items-center mb-6">
-              <div class="md:w-1/3">
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
                 <label
-                  class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                  for="name"
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  htmlFor="role"
                 >
                   Role
                 </label>
               </div>
-              <div class="md:w-2/3">
+              <div className="md:w-2/3">
                 <input
-                  class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                  id="name"
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="role"
                   type="text"
-                  value="Product Engineer"
+                  value={role}
+                  onChange={(e) => {
+                    setRole(e.target.value);
+                  }}
                 />
               </div>
             </div>
