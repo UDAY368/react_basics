dc6eced605484319b0421ac90da9e51c1e54e353
UDAY368 <udayabdm368@gmail.com>
2023-07-01 11:37:16 +0530

    Add the model and added styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import { useState } from 'react';
-import './index.css';
-import Employee from './components/Employee';
-
+import { useState } from "react";
+import "./index.css";
+import Employee from "./components/Employee";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   // Perfect usage of the useState Hook
-  const [role , setRole] = useState("dev")
-  const isShowEmployee = true
+  const [employees, setemployees] = useState([
+    {
+      name: "uday kumar",
+      role: "product Engineer",
+      img: "https://images.pexels.com/photos/2955376/pexels-photo-2955376.jpeg",
+    },
+    {
+      name: "srinu Uppati",
+      role: "Product Marketing",
+      img: "https://images.pexels.com/photos/756484/pexels-photo-756484.jpeg",
+    },
+    {
+      name: "Shakeel",
+      role: "production Lead",
+      img: "https://images.pexels.com/photos/2955376/pexels-photo-2955376.jpeg",
+    },
+    {
+      name: "Usha Rani",
+      role: "Front End Developer",
+      img: "https://images.pexels.com/photos/7897404/pexels-photo-7897404.jpeg",
+    },
+    {
+      name: "Samyuktha Sammu",
+      role: "HR Relations",
+      img: "https://images.pexels.com/photos/7897404/pexels-photo-7897404.jpeg",
+    },
+    {
+      name: "Prasanna prassu",
+      role: "Java Developer",
+      img: "https://images.pexels.com/photos/7897404/pexels-photo-7897404.jpeg",
+    },
+    {
+      name: "Shankar",
+      role: "python Developer",
+      img: "https://images.pexels.com/photos/2955376/pexels-photo-2955376.jpeg",
+    },
+    {
+      name: "Prasad",
+      role: "employeee Relations",
+      img: "https://images.pexels.com/photos/2955376/pexels-photo-2955376.jpeg",
+    },
+    {
+      name: "Chiranjeevi",
+      role: "CEO",
+      img: "https://images.pexels.com/photos/2955376/pexels-photo-2955376.jpeg",
+    },
+  ]);
+  const isShowemployee = true;
   return (
-    <div className='App'>
-      
-      { isShowEmployee ? <>
-        <input type='text' placeholder="Enter Role" className='p-1' onChange={(e) => {
-          setRole(e.target.value)
-        }}/>
-       <Employee name = "Uday Kumar" role = "Product Engineer"/>
-       <Employee name = "Srinu" role = {role}/>
-       <Employee name = "Shakeel"/>
-      </> : <p>There is no Employee to Display</p>}
+    <div className="App">
+      {isShowemployee ? (
+        <>
+          {/* <input
+            type="text"
+            placeholder="Enter Role"
+            className="p-1"
+            onChange={(e) => {
+              setRole(e.target.value);
+            }}
+          /> */}
+          <div className=" flex flex-wrap justify-center">
+            {employees.map((employee) => {
+              return (
+                <Employee
+                  key={uuidv4()}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                />
+              );
+            })}
+          </div>
+        </>
+      ) : (
+        <p>There is no employeee to Display</p>
+      )}
     </div>
   );
 }
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import EditEmployee from "./EditEmployee";
 
 function Employee(props) {
   return (
-    <div>
-      <h3 className="text-lg font-bold underline">Employee {props.name}</h3>
-      {props.role ? <p>{props.role}</p> : <p>No Role Available</p>}
+    <div className=" m-2 py-8 px-8 max-w-sm  bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <img
+        className=" object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 sm:mx-0 sm:shrink-0"
+        src={props.img}
+        alt="EmployeeImage"
+      />
+      <div className="text-center space-y-2 sm:text-left">
+        <div className="space-y-0.5">
+          <p className="text-lg text-black font-semibold">{props.name}</p>
+          <p className="text-slate-500 font-medium">
+            {props.role ? props.role : "No role"}
+          </p>
+        </div>
+        <EditEmployee />
+      </div>
     </div>
-  )
+  );
 }
-export default Employee
+export default Employee;
